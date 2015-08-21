be7992fb1a533bdecb980f6d97dbada10206ecc7
Lenau87 <joker8725@googlemail.com>
2015-08-21 13:15:52 +0200

    Final

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,31 @@
+## In the beginning inv is a 1 by 1 matrix that contains only one value (NA)
+## It will be rewritten by the inverse matrix of x
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- matrix()
+  set <- function(y) {
+    x <<- y
+    inv <<- matrix()
+  }
+  get <- function() x
+  setinv <- function(solve) inv <<- solve
+  getinv <- function() inv
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
+## Since we assume that the matrix x is invertible, the inverse can not contain NAs.
+## I used that as a test to check if the inverse was already stored.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.na(inv[1,1])) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinv(inv)
+  inv
 }
